2b26b3c208a73887cea6a27fbebf7eddf268276a
elvis <elvisotieno24@gmail.com>
2025-04-19 19:43:16 +0300

    adds files contet

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,22 @@
+
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" end>
+        Home
+      </NavLink>
+      <NavLink to="/directors" end>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" end>
+        Actors
+      </NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,38 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then(r => r.json())
+      .then(data => setMovies(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        <ul>
+          {movies.map(movie => (
+            <li key={movie.id}>
+              <MovieCard id={movie.id} title={movie.title} />
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
-};
+}
 
 export default Home;
+
+
+
+
